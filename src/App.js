b2c356c70844d67043465f50e51bbc79a78a0cdf
Tim Finnigan <tim.finnigan1@gmail.com>
2020-01-01 12:24:58 -0500

    update portfolio details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 class App extends React.Component {
   render() {
     return (
@@ -6,8 +6,27 @@ class App extends React.Component {
         <br />
         {/* <h4>About</h4> */}
         <p>I like to create simple, practical applications.</p>
+        <p><strong>Specializing in: </strong>JavaScript, HTML, CSS</p>
+        <p><strong>Experienced with: </strong>Angular, React, Highcharts</p>
+        <div id="portfolio-links">
+          <a
+            href="https://github.com/TimFinnigan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/timfinnigan11/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a>Resume</a>
+        </div>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
