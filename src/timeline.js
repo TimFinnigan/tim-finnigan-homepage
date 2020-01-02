90cf3f7bb4aff3e6befa8c1c8706458fa9083d04
Tim Finnigan <tim.finnigan1@gmail.com>
2020-01-02 11:11:38 -0500

    update timeline

--- src/timeline.js
+++ src/timeline.js
@@ -6,7 +6,7 @@ class Timeline extends Component {
       <div className="timeline-wrapper">
         <ul className="timeline">
           <li className="timeline-item is-done">
-            <div className="bold time">2018</div>
+            <div className="bold time">2017-20</div>
             <div className="bold">Software Engineer at Siemens</div>
             <div>TODO: add more details here...</div>
           </li>
@@ -23,7 +23,7 @@ class Timeline extends Component {
             <div className="bold">Worked at Startups</div>
           </li>
           <li className="timeline-item is-done">
-            <div className="bold time">2013</div>
+            <div className="bold time">2013-14</div>
             <div className="bold">Taught English in Taiwan</div>
           </li>
           <li className="timeline-item is-done">
