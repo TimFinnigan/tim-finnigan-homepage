c5461eec3c733264a7e008bf7fc345551e8ee4af
Tim Finnigan <tim.finnigan1@gmail.com>
2020-01-11 13:09:27 -0500

    add links to projects

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,22 @@
 import React from "react";
 import { Route, NavLink } from "react-router-dom";
-const Project = ({ match }) => <p>{match.params.id}</p>;
+// const Project = ({ match }) => <p>{match.params.id}</p>;
+const Data = ({ match }) => <p>This website is currently in progress</p>;
+const Gantt = ({ match }) => (
+  <a
+    href="http://ganttchartmaker.com"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Gantt Chart Maker
+  </a>
+);
+const Map = ({ match }) => (
+  <a href="http://worldmapquiz.com" target="_blank" rel="noopener noreferrer">
+    World Map Quiz
+  </a>
+);
+
 class Projects extends React.Component {
   render() {
     const { url } = this.props.match;
@@ -18,7 +34,10 @@ class Projects extends React.Component {
             <NavLink to="/projects/worldmapquiz">World Map Quiz</NavLink>
           </li>
         </ul>
-        <Route path="/projects/:id" component={Project} />
+        {/* <Route path="/projects/:id" component={Project} /> */}
+        <Route exact path="/projects/datashown" component={Data} />
+        <Route exact path="/projects/ganttchartmaker" component={Gantt} />
+        <Route exact path="/projects/worldmapquiz" component={Map} />
       </div>
     );
   }
