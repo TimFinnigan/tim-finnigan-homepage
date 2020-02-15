e3416309b5ad6cd6284849a7161565c67135df4b
Tim Finnigan <tim.finnigan1@gmail.com>
2020-02-15 11:44:00 -0500

    add project descriptions

--- src/projects.js
+++ src/projects.js
@@ -1,31 +1,58 @@
 import React from "react";
 import { Route, NavLink } from "react-router-dom";
 // const Project = ({ match }) => <p>{match.params.id}</p>;
-const Data = ({ match }) => <p>This website is currently in progress</p>;
+const Data = ({ match }) => (
+  <div className="project-description">
+    <a
+      href="http://datashown.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="project-link"
+    >
+      Data Shown
+    </a>
+    <ul>
+      <li>Visual essays covering a variety of topics</li>
+      <li>Built using Angular</li>
+    </ul>
+  </div>
+);
 const Gantt = ({ match }) => (
-  <a
-    href="http://ganttchartmaker.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="project-link"
-  >
-    Gantt Chart Maker
-  </a>
+  <div className="project-description">
+    <a
+      href="http://ganttchartmaker.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="project-link"
+    >
+      Gantt Chart Maker
+    </a>
+    <ul>
+        <li>A tool for easily creating Gantt charts</li>
+        <li>Built using Highcharts</li>
+    </ul>
+  </div>
 );
 const Map = ({ match }) => (
-  <a
-    href="http://worldmapquiz.com"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="project-link"
-  >
-    World Map Quiz
-  </a>
+  <div className="project-description">
+    <a
+      href="http://worldmapquiz.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="project-link"
+    >
+      World Map Quiz
+    </a>
+    <ul>
+      <li>A game where users try to guess countrys</li>
+      <li>Built using Amcharts library</li>
+    </ul>
+  </div>
 );
 
 class Projects extends React.Component {
   render() {
-    const { url } = this.props.match;
+    // const { url } = this.props.match;
     return (
       <div className="project-container">
         <strong>Select a Project</strong>
