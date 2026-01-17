836f0137c134ab94f2780d4ce2b2ac542d4f9255
TimFinnigan <TimFinnigan@users.noreply.github.com>
2026-01-17 11:10:11 -0800

    refactor App.js and index.css: enhance layout and styling, update skills and links, and improve responsiveness

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import React from 'react';
 import Timeline from './timeline.js';
+
 class App extends React.Component {
 	render() {
 		return (
 			<div className='flex-container'>
-				<span id='about'>
+				<div id='about'>
 					<strong>About</strong>
-					<p>Efficient software engineer and problem solver.</p>
 					<p>
-						Positive attitude and easy to work with. Always looking
-						to learn.
+						A curious and efficient software engineer who enjoys solving problems and working with others. Always learning, always improving, and excited about using AI to build better software.
 					</p>
-					<p>
-						Interested in using AI to build better software.
-					</p>
+
 					<strong>Skills</strong>
-					<br />
-					<ul className='skills'>
-						<li>AWS</li>
-						<li>Python</li>
-						<li>JavaScript</li>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>React</li>
-					</ul>
-					<ul className='skills'>
-						<li>Angular</li>
-						<li>Node.js</li>
-						<li>SQL</li>
-						<li>Tableau</li>
-						<li>Highcharts</li>
-						<li>Chart.js</li>
-					</ul>
-					<br />
+					<div>
+						<ul className='skills'>
+							<li>AWS</li>
+							<li>Python</li>
+							<li>JavaScript</li>
+							<li>HTML</li>
+							<li>CSS</li>
+							<li>React</li>
+							<li>Angular</li>
+							<li>Node.js</li>
+							<li>SQL</li>
+							<li>Tableau</li>
+							<li>Highcharts</li>
+							<li>Chart.js</li>
+						</ul>
+					</div>
+
 					<strong>Links</strong>
-					<br />
-					<span id='portfolio-links'>
-						<a
-							href='https://github.com/tim-finnigan'
-							target='_blank'
-							rel='noopener noreferrer'
-						>
-							GitHub
-						</a>
+					<div id='portfolio-links'>
 						<a
 							href='https://www.linkedin.com/in/tim-finnigan/'
 							target='_blank'
@@ -50,22 +38,26 @@ class App extends React.Component {
 						>
 							LinkedIn
 						</a>
-						{/* <a
-              href="https://twitter.com/tim_finnigan1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a> */}
-						{/* <a>Resume</a> */}
-					</span>
-					{/* <strong>Email</strong>
-					<br />
-					<span>tim.finnigan1@gmail.com</span> */}
-				</span>
+						<a
+							href='https://github.com/tim-finnigan'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							GitHub (Work)
+						</a>
+						<a
+							href='https://github.com/TimFinnigan/'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							GitHub (Personal)
+						</a>
+					</div>
+				</div>
 				<Timeline />
 			</div>
 		);
 	}
 }
+
 export default App;
